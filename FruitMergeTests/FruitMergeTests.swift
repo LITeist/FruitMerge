349805f2dfc2120ac474f11b52fc9c86bfe454df
Alexey Levanov <alexeylevano27@gmail.com>
2024-11-23 19:54:38 +0200

    Initial Commit

---
+++ FruitMergeTests/FruitMergeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMergeTests.swift
+//  FruitMergeTests
+//
+//  Created by Леванов Алексей on 23.11.2024.
+//
+
+import Testing
+@testable import FruitMerge
+
+struct FruitMergeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
